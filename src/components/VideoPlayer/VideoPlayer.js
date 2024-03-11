93d7d403f3bbf112733bbc51224e037856dd990f
Jovan <Qeleshi.Jovan@gmail.com>
2024-03-10 22:34:33 -0400

    Finish video player componenet

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -6,30 +6,43 @@ import heartIcon from "../../assets/Icons/likes.svg"
 function VideoPlayer() {
     return (
         <>
-            <div className="videoComponenet">
-                <video className="videoComponenet-controller" controls poster={posterImage} />
+            <div className="videoComponent">
+                <video className="videoComponent-controller" controls poster={posterImage} />
 
                 <div className="videoComponent-infoContainer">
 
-                    <div className="videoComponent-infoContainer-titleInfo">
+                    <div className="videoComponent-infoContainer-title">
                         <h1>The Future of Artifical Intelligence</h1>
                     </div>
 
-                    <div className="videoComponent-infoContainer-titleInfo">
-                        <h3>By Aiden Thompson</h3>
-                        <p>08/08/2023</p>
-                        <p>980,544</p>
-                        <p>22,479</p>
+                    <div className="videoComponent-infoContainer-details">
+
+                        <div className="videoComponent-infoContainer-details-author">
+                            <h3>By Aiden Thompson</h3>
+                            <p>08/08/2023</p>
+                        </div>
+                        <div className="videoComponent-infoContainer-details-viewsLikes">
+                            <div>
+                                <div>
+                                    <img src={viewIcon} />
+                                    <div></div>
+                                    <p>980,544</p>
+                                </div>
+                                <div>
+                                    <img src={heartIcon} />
+                                    <div></div>
+                                    <p>22,479</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="videoComponent-infoContainer-fullInfo">
-                        <p>Explore the cutting-edge developments and predictions for Artificial Intelligence in the coming years. 
-                            From revolutionary breakthroughs in machine learning to the ethical considerations influencing AI advancements, 
-                            this exploration transcends the boundaries of mere speculation. Join us on a journey that navigates the intricate 
+                        <p>Explore the cutting-edge developments and predictions for Artificial Intelligence in the coming years.
+                            From revolutionary breakthroughs in machine learning to the ethical considerations influencing AI advancements,
+                            this exploration transcends the boundaries of mere speculation. Join us on a journey that navigates the intricate
                             interplay between innovation, ethics, and the ever-evolving tech frontier.</p>
                     </div>
-
-
                 </div>
 
             </div>
