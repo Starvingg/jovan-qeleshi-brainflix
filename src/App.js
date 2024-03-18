82c06ebf5522b5fc124a65ce4e663fad295047a9
Jovan <Qeleshi.Jovan@gmail.com>
2024-03-17 20:10:01 -0400

    Final Commit to merge to develop

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
+/// api_key: "77e0d206-dee2-4ac6-a718-50ed76a85a99"
+
 import './app.scss'
+
 import React, { useState } from 'react';
-
-import CreateComment from './components/Comments/CreateComment/CreateComment';
-import DisplayComments from './components/Comments/DisplayComment/DisplayComment';
 import NavBar from './components/NavBar/NavBar';
-import RecommendedVideos from './components/RecommendedVideos/RecommendedVideos';
-import VideoPlayer from './components/VideoPlayer/VideoPlayer';
-
 import videoData from "./data/video-details.json"
-import VideoInfo from './components/VideoInfo/VideoInfo';
-
 import HomePage from './pages/HomePage/HomePage';
 
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
-  const [selectedVideo, setSelectedVideo] = useState(videoData[0]);
 
   return (
     <>
